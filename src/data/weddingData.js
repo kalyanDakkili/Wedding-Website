a120f33aaf1b1ceb25370fb41bf3d5219841ac00
Kalyan Dakkili <kalyandakkili2@gmail.com>
2026-08-17 18:23:04 +0530

    Fix venue address in weddingData.js

--- src/data/weddingData.js
+++ src/data/weddingData.js
@@ -25,7 +25,7 @@ export const groom = {
 
 export const venue = {
   name: "Gandhamaneni Sivaiah Krishna Reddy Bhavan",
-  address: "Near C.P.B. Office Prakkana, Bairagi patteda, Tirupati, Andhra Pradesh",
+  address: "Near C.P.I. Office Prakkana, Bairagi patteda, Tirupati, Andhra Pradesh",
   mapsQuery:
     "Sri+Gandhamaneni+Sivayya+Krishna+Reddy+Bhavan+Bhairogipalem+Tirupati",
   mapsEmbed:

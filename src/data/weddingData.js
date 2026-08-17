ebf8b1a9042fe858753b7bb90a00cb77cc53d600
Kalyan Dakkili <kalyandakkili2@gmail.com>
2026-08-17 18:31:27 +0530

    Update weddingData.js

--- src/data/weddingData.js
+++ src/data/weddingData.js
@@ -24,7 +24,7 @@ export const groom = {
 };
 
 export const venue = {
-  name: "sri Gandhamaneni Sivaiah Krishna Reddy Bhavan",
+  name: "Sri Gandhamaneni Sivaiah Krishna Reddy Bhavan",
   address: "Near C.P.I. Office Prakkana, Bairagi patteda, Tirupati, Andhra Pradesh",
   mapsQuery:
     "Sri+Gandhamaneni+Sivayya+Krishna+Reddy+Bhavan+Bhairogipalem+Tirupati",
